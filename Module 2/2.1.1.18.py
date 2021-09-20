c838c0d961c51786410f0af331cffd8330d15a25
JamesDevaney1150 <90684373+JamesDevaney1150@users.noreply.github.com>
2021-09-20 09:21:14 +0100

    Delete 2.1.1.18.py

--- Module 2/2.1.1.18.py
+++
@@ -1,4 +0,0 @@
-#Modify the first line of code in the editor, using the sep and end keywords, to match the expected output
-print("Programming","Essentials","in...",sep="***",end="")
-#Don't change anything in the second print() invocation.
-print("Python")
