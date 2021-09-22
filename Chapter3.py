f1bbea3d9736ee9cd42e1a265fb234693ab05f4f
James Devaney <Jim Devaney>
2021-09-22 19:42:15 +0100

    Labs completed up to 3.2.1.15

--- Chapter3.py
+++ Chapter3.py
@@ -122,14 +122,11 @@ print("The height of the pyramid:", height)
 #Familiarise using the while loop
 
 c0 = int(input("Enter a number: "))
-steps = 0
-if c0 > 1: #take any non-negative and non-zero integer number and name it c0;
-	while c0 != 1: #if c0 ≠ 1, skip to point 2
-		if c0 %2 != 0:c1 = 3 * c0 + 1 #otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1
-		else:c1 = c0 // 2 #f it's even, evaluate a new c0 as c0 ÷ 2
-		print(c0)
-		c0 = c1
-		steps += 1 #step counter, counts the number of loops by incrementing 1 everyone loop
-	print("steps =",steps)
-else:
-	print("¯\_(ツ)_/¯ Has to be above non-negative/zero")
+if c0 < 1: print("¯\_(ツ)_/¯ Can't be a non-negative/zero") #take any non-negative and non-zero integer number and name it c0;
+else: steps=0
+while c0 != 1: #if c0 ≠ 1, skip to point 2
+	if c0 %2 == 0: c0=int(c0/2) #otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1 #if it's even, evaluate a new c0 as c0 ÷ 2
+	else:c0=3*c0+1 #otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1
+	print(c0)
+	steps += 1 #step counter, counts the number of loops by incrementing 1 everyone loop
+print("steps =",steps)
